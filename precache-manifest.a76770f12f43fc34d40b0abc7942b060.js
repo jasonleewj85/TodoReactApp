1f6e791426ec88a85265b1acbf4bbfa26ba8d80c
Jason Lee <lee.wei.jun@rhbgroup.com>
2020-09-10 15:23:50 +0800

    Updates

---
+++ precache-manifest.a76770f12f43fc34d40b0abc7942b060.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92978e9e1663e1840b61a546f2145006",
+    "url": "/TodoReactApp/index.html"
+  },
+  {
+    "revision": "27ad11a052df1d1f820d",
+    "url": "/TodoReactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "048f01822becaf312f8a",
+    "url": "/TodoReactApp/static/js/2.686e006a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoReactApp/static/js/2.686e006a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ad11a052df1d1f820d",
+    "url": "/TodoReactApp/static/js/main.ae9a0d81.chunk.js"
+  },
+  {
+    "revision": "ab96d7bd9908d603ae0c",
+    "url": "/TodoReactApp/static/js/runtime-main.7590630c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TodoReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
