84f5205adf2d7790e33ac9b01bc9eacecec6474b
Jason Lee <lee.wei.jun@rhbgroup.com>
2020-09-10 15:43:07 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faPlus, faEdit } from "@fortawesome/free-solid-svg-icons";
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Demo from './components/Demo';
+import Demo1 from './components/Demo1';
+import Parent from './components/Parent';
+import Sample from './components/Sample';
+import TodoList from './components/ToDoList';
+
+library.add(faTrash, faEdit, faPlus);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>App component</h1> */}
+      <TodoList></TodoList>
     </div>
   );
 }
